3e9c2d01a7045cf3541bba36eb6a51cd86d5b843
alessiaBer <alexdeyn@gmail.com>
2023-06-05 16:58:27 +0200

    [populate] migration and seeder

--- database/migrations/2023_06_05_140150_create_projects_table.php
+++ database/migrations/2023_06_05_140150_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150);
+            $table->string('slug', 255);
+            $table->text('description');
+            $table->string('project_image', 255);
+            $table->string('project_url', 255)->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,18 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) {
+
+            $project = new Project();
+            $project->title = $faker->sentence(4);
+            $project->slug = Str::slug($project->title, '-');
+            $project->description = $faker->text();
+            $project->project_image = $faker->imageUrl();
+            $project->project_url = $faker->url();
+            $project->save();
+        }
+
     }
 }
