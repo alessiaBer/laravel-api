2594acd4236a8906540aa72034d015ac4e996e9e
alessiaBer <alexdeyn@gmail.com>
2023-06-05 17:14:42 +0200

    [add] table to projects.index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.admin')
 
 @section('content')
-<h1>Ciao</h1>
+<div class="container">
+    <h2>{{__('Projects')}}</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Image</th>
+                <th scope="col">Url</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($projects as $project)
+            <tr>
+                <th scope="row">{{$project->id}}</th>
+                <td>{{$project->title}}</td>
+                <td>{{$project->project_image}}</td>
+                <td>{{$project->project_url}}</td>
+                <td>{{$project->slug}}</td>
+                <td>view|edit|delete</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
