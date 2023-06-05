e4cadda84bc3eb9629ca7e3d555ac56444e771cd
alessiaBer <alexdeyn@gmail.com>
2023-06-05 19:07:36 +0200

    add show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.admin')
 
 @section('content')
-
+<section>
+<div class="container py-5">
+    <h3 class="pb-4">Title: {{$project->title}}</h3>
+    <div class="content_container d-flex">
+        <div class="project_img_container">
+            <img src="{{$project->project_image}}" alt="{{$project->title}}" height="300">
+        </div>
+        <div class="project_info ps-4">
+            <p><strong>Description:</strong><br>{{$project->description}}</p>
+            <span class="d-block"><strong>Link: </strong>{{$project->project_url}}</span>
+        </div>
+    </div>
+</div>
+</section>
 @endsection
