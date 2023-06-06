eea7325118b8dd17cf36fde80b5647b081ebdb4b
alessiaBer <alexdeyn@gmail.com>
2023-06-06 11:38:41 +0200

    [fix] delete

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -45,18 +45,18 @@
                     </a>
                     <!-- Button trigger modal -->
                     <button type="button" class="btn btn-danger actions" data-bs-toggle="modal"
-                        data-bs-target="#modal{{$project->id}}">
+                        data-bs-target="#modal{{$project->slug}}">
                         Delete
                     </button>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="modal{{$project->id}}" tabindex="-1"
-                        aria-labelledby="modalTitle-{{$project->id}}" aria-hidden="true">
+                    <div class="modal fade" id="modal{{$project->slug}}" tabindex="-1"
+                        aria-labelledby="modalTitle-{{$project->slug}}" aria-hidden="true">
                         <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
+                            <div class="modal-content bg-dark">
+                                <div class="modal-header border-0">
                                     <h1 class="modal-title fs-5" id="exampleModalLabel">Hey bro!</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
@@ -66,7 +66,7 @@
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary"
                                         data-bs-dismiss="modal">Close</button>
-                                    <form action="{{route('admin.projects.destroy', $project->id)}}" method="post"
+                                    <form action="{{route('admin.projects.destroy', $project->slug)}}" method="post"
                                         class="d-inline-block">
                                         @csrf
                                         @method('DELETE')
