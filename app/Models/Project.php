93330cb2ab270d09c63104e4f43cc96dd5606f47
alessiaBer <alexdeyn@gmail.com>
2023-06-07 17:05:59 +0200

    [add] relation belongsTo

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -15,5 +16,10 @@ class Project extends Model
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    public function type() :BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
