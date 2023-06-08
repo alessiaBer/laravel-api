cdfe60d11b6a37384082056097d53856f1a1c103
alessiaBer <alexdeyn@gmail.com>
2023-06-08 16:24:40 +0200

    [add] relation belongsToMany to the models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -18,8 +19,14 @@ class Project extends Model
         return Str::slug($title, '-');
     }
 
-    public function type() :BelongsTo
+    public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+
 }
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Technology extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'tech_img_url'];
+
+    public static function generateSlug($name)
+    {
+        return Str::slug($name, '-');
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
+    }
+    
 }
