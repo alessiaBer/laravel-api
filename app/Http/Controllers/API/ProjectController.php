e4ff4c114158930e8326cce62182607156e450b9
alessiaBer <alexdeyn@gmail.com>
2023-08-31 20:44:06 +0200

    [fix] view of projects

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -9,7 +9,7 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::with(['type', 'technologies', 'user'])->orderByDesc('id')->get();
+        $projects = Project::with(['type', 'technologies', 'user'])->orderByAsc('id')->get();
 
         return response()->json([
             'success' => true,
